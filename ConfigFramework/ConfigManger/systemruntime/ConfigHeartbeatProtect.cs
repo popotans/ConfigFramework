43baadd5088de705b7116004b079df875b154aa7
zwc00zwc <1484659036@qq.com>
2016-01-22 15:29:23 +0800

    init
    init

--- ConfigFramework/ConfigManger/systemruntime/ConfigHeartbeatProtect.cs
+++ ConfigFramework/ConfigManger/systemruntime/ConfigHeartbeatProtect.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using ZF.IOHelper;
 
 namespace ConfigFramework.ConfigManger.systemruntime
 {
@@ -15,35 +16,124 @@ namespace ConfigFramework.ConfigManger.systemruntime
     /// </summary>
     public class ConfigHeartbeatProtect : IDisposable
     {
-        private static ConfigHeartbeatProtect configHeartbeat;
+        private static ConfigHeartbeatProtect configHeartbeat = null;
         private static CancellationTokenSource cancelSource;
-        private object _lockheart = new object();
-        private object _lockconfig = new object();
+        private static object _lockheart = new object();  //单例锁
+        private static object _lockconfig = new object(); //更新锁
 
         public ConfigHeartbeatProtect()
         {
-            
+            try
+            {
+                cancelSource = new CancellationTokenSource();
+
+                System.Threading.Tasks.Task.Factory.StartNew(() =>
+                {
+                    HeartRun();
+                }, cancelSource.Token);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+
         }
 
+        public void HeartRun()
+        {
+            while (!cancelSource.IsCancellationRequested)
+            {
+                System.Threading.Thread.Sleep(10 * 1000);
+                try
+                {
+                    LoadConfig(false);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+        }
+
+        /// <summary>
+        /// 单例
+        /// </summary>
+        /// <returns></returns>
         public static ConfigHeartbeatProtect Instance()
         {
             if (configHeartbeat == null)
             {
-                return new ConfigHeartbeatProtect();
+                lock (_lockheart)
+                {
+                    configHeartbeat = new ConfigHeartbeatProtect();
+                }
             }
-            else
+            return configHeartbeat;
+        }
+
+        /// <summary>
+        /// 是否重新下载项目信息
+        /// </summary>
+        /// <param name="updatejson">是否重新下载配置分类</param>
+        public void LoadConfig(bool isload)
+        {
+            lock (_lockconfig)
             {
-                return configHeartbeat;
+                string jasonpath = AppDomain.CurrentDomain.BaseDirectory + "\\json.text";
+                try
+                {
+                    bool isupdatelocal = false;
+                    DateTime updatetime = DateTime.Now;
+                    
+                    if (isload)
+                    {
+
+                    }
+                    else
+                    {
+                        ConfigDal configdal = new ConfigDal();
+                        long[] cids = AppDomainContext.Context.CategoryModels.Select(p => p.Id).ToArray();
+
+                        List<Config> configs = configdal.GetListByCategoryIds(cids, updatetime);
+                        List<ConfigModel> configmodels = ToConfigModel(configs);
+                        foreach (var item in configmodels)
+                        {
+                            AppDomainContext.Context.ConfigInfoOfKeyDic.SetConfig(item);
+                        }
+                        if (configs.Count > 0)  //本次有配置更新
+                        {
+                            isupdatelocal = true;
+                        }
+                    }
+
+                    if (isupdatelocal)
+                    {
+                        string json1 = JsonConvert.SerializeObject(AppDomainContext.Context);
+                        IOHelper.Write(jasonpath, json1);  //写入磁盘
+                    }
+                }
+                catch (Exception)
+                {
+                    string json2 = IOHelper.Read(jasonpath);
+                    JsonConvert.DeserializeObject<ConfigContext>(json2);   //从磁盘获得上次正确的配置
+                }
             }
         }
 
-        public void LoadConfig(bool updatejson)
+        //Config信息转换到ConfigModel   通过ConfigModel可配置故障转移配置轮询等
+        public List<ConfigModel> ToConfigModel(List<Config> configs)
         {
-            lock (_lockconfig)
-            { 
-            
+            List<ConfigModel> modellist = new List<ConfigModel>();
+            foreach (var item in configs)
+            {
+                ConfigModel model = new ConfigModel();
+                model.config = item;
+                modellist.Add(model);
             }
+            return modellist;
         }
+
+        
 
         public void Dispose()
         {
